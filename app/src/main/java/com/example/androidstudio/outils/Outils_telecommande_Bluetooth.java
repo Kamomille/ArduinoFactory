76d5a3ef4f6fd7a576547a47d308aab8124069f3
user name <user email>
2021-04-09 06:54:25 +0200

    Changement nom d'une page Juju

--- app/src/main/java/com/example/androidstudio/outils/Outils_telecommande_Bluetooth.java
+++ app/src/main/java/com/example/androidstudio/outils/Outils_telecommande_Bluetooth.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.androidstudio.R;
 
-public class Outils_telecommande extends AppCompatActivity {
+public class Outils_telecommande_Bluetooth extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outils_telecommande);
+        setContentView(R.layout.activity_outils_telecommande__bluetooth);
     }
 }
--- app/src/main/java/com/example/androidstudio/pages/Page_Outils.java
+++ app/src/main/java/com/example/androidstudio/pages/Page_Outils.java
@@ -5,16 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.androidstudio.outils.Outils_resistance;
-import com.example.androidstudio.outils.Outils_telecommande;
 import com.example.androidstudio.R;
+import com.example.androidstudio.outils.Outils_telecommande_Bluetooth;
 
 public class Page_Outils extends AppCompatActivity {
 
-    private LinearLayout outil_resistance, outil_telecommande;
+    private LinearLayout outil_resistance, outil_telecommande_bluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +24,8 @@ public class Page_Outils extends AppCompatActivity {
         outil_resistance.setOnClickListener(new View.OnClickListener(){
             @Override public void onClick(View v){ openActivtity_outilsResistance(); } } );
 
-        outil_telecommande = (LinearLayout) findViewById(R.id.outil_telecommande);
-        outil_telecommande.setOnClickListener(new View.OnClickListener(){
+       outil_telecommande_bluetooth = (LinearLayout) findViewById(R.id.outil_telecommande);
+        outil_telecommande_bluetooth.setOnClickListener(new View.OnClickListener(){
             @Override public void onClick(View v){ openActivtity_outilsTelecommande(); } } );
     }
 
@@ -35,7 +34,7 @@ public class Page_Outils extends AppCompatActivity {
         startActivity(intent);
     }
     public void openActivtity_outilsTelecommande(){
-        Intent intent = new Intent(this, Outils_telecommande.class);
+        Intent intent = new Intent(this, Outils_telecommande_Bluetooth.class);
         startActivity(intent);
     }
 
