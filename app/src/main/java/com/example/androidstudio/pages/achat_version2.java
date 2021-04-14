3a41e386f9910b21558bc6d2552988330db65763
Kamomille <camille.bayon-de-noyer@esme.fr>
2021-04-14 23:32:44 +0200

    Amélioration barre de recherche (partie achat)

--- app/src/main/java/com/example/androidstudio/pages/achat_version2.java
+++ app/src/main/java/com/example/androidstudio/pages/achat_version2.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Filter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -34,6 +35,67 @@ public class achat_version2 extends AppCompatActivity {
 
     ArrayList<String> list;
     ArrayAdapter<String > adapter;
+
+
+    String[][] listeMateriel = {
+            {"Kit Elegoo"},
+            {"Carte Arduino"},
+            {"Capteur distance"},
+            {"Bouton poussoir"},
+            {"Joystick"},
+            {"Capteur de son"},
+            {"Buzzer"},
+            {"Shock"},
+            {"Photorésistance"},
+            {"Humidité"},
+            {"Motorshield"},
+            {"Carte méga"},
+            {"Carte nano"},
+            {"Carte raspberry"},
+            {"Kit rapsberry"},
+            {"Machine à souder"},
+            {"Etain"},
+            {"Kit soudure"},
+            {"Dénudeur de fil"},
+            {"Moteur dc"},
+            {"Servomoteur"},
+            {"Led"},
+            {"Breadboard"},
+            {"Prototype"},
+            {"Module wifi"},
+            {"Module bluetooth"}
+    };
+
+    int[] listeMateriel2 = {
+            R.drawable.achat_kit,
+            R.drawable.achat_carte_arduino,
+            R.drawable.achat_distance,
+            R.drawable.achat_bouton,
+            R.drawable.achat_joystick,
+            R.drawable.achat_capteur_son,
+            R.drawable.achat_buzzer,
+            R.drawable.achat_shock,
+            R.drawable.achat_photoresistance,
+            R.drawable.achat_humidite,
+            R.drawable.achat_motorshield,
+            R.drawable.achat_carte_mega,
+            R.drawable.achat_carte_nano,
+            R.drawable.achat_raspberry,
+            R.drawable.achat_kit_raspberry,
+            R.drawable.achat_machine_soudure,
+            R.drawable.achat_etain,
+            R.drawable.achat_kit_soudure,
+            R.drawable.achat_pince_soudure,
+            R.drawable.achat_moteur_dc,
+            R.drawable.achat_servomoteur,
+            R.drawable.achat_led,
+            R.drawable.achat_breadboard,
+            R.drawable.prototype,
+            R.drawable.achat_module_wifi,
+            R.drawable.achat_module_bluetooth
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,52 +113,28 @@ public class achat_version2 extends AppCompatActivity {
         layout_recherche = (LinearLayout) findViewById(R.id.layout_recherche);
 
         list = new ArrayList<>();
-        list.add("Kit Elegoo");
-        list.add("Carte Arduino");
-        list.add("Capteur distance");
-        list.add("Bouton poussoir");
-        list.add("Joystick");
-        list.add("Capteur de son");
-        list.add("Buzzer");
-        list.add("Shock");
-        list.add("Photorésistance");
-        list.add("Humidité");
-        list.add("Motorshield");
-        list.add("Carte méga");
-        list.add("Carte nano");
-        list.add("Carte raspberry");
-        list.add("Kit rapsberry");
-        list.add("Machine à souder");
-        list.add("Etain");
-        list.add("Kit soudure");
-        list.add("Dénudeur de fil");
-        list.add("Moteur dc");
-        list.add("Servomoteur");
-        list.add("Led");
-        list.add("Breadboard");
-        list.add("Prototype");
-        list.add("Module wifi");
-        list.add("Module bluetooth");
+        for (int i = 0; i < listeMateriel.length; i++) { // recuperation des noms des composants
+            list.add(listeMateriel[i][0]);
+        }
+
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
         listView.setTextFilterEnabled(true);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                searchView.setQueryHint("test");
+                //String text = String.valueOf(listeMateriel[position]);
+                //searchView.setQueryHint(text);
             }
         });
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-
                 if(list.contains(query)){
                     adapter.getFilter().filter(query);
-                    //affichage_recherche(query, adapter);
                 }else{
                     Toast.makeText(achat_version2.this, "No Match found",Toast.LENGTH_LONG).show();
                 }
@@ -113,7 +151,6 @@ public class achat_version2 extends AppCompatActivity {
                 else{
                     affichage_recherche(newText, adapter);
                 }
-
                 return false;
             }
 
@@ -143,26 +180,27 @@ public class achat_version2 extends AppCompatActivity {
         }
         else {
             for (int i = 0; i < adapter.getCount(); i++) {
+
+                ImageButton imageButton = new ImageButton(this);
+                Drawable drawable = getResources().getDrawable(listeMateriel2[i]);
+                imageButton.setImageDrawable(drawable);
+                imageButton.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageButton.setBackgroundColor(parseColor("#00000000"));
+                layout_recherche.addView(imageButton);
+
                 Button button = new Button(this);
                 button.setText(adapter.getItem(i).toString());
                 button.setBackgroundColor(parseColor("#0B789C"));
                 button.setTextColor(parseColor("#FFFFFF"));
                 layout_recherche.addView(button);
 
-                ImageButton imageButton = new ImageButton(this);
-                imageButton.setImageDrawable(Drawable.createFromPath("@drawable/achat_bouton"));
-                layout_recherche.addView(imageButton);
             }
         }
 
-        //setContentView(button);
         //button.setLayoutParams(new LinearLayout.LayoutParams
          //       (LinearLayout.LayoutParams.WRAP_CONTENT,
          //               LinearLayout.LayoutParams.MATCH_PARENT));
 
-        //button.setId(1);
-
-        //button.setText("XX");
 
         return true;
     }
