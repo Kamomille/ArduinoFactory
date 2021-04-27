caa03ede49c0bc2ccef60a46091a18f8b498e73d
Julien Gouban <julien.gouban@esme.fr>
2021-04-27 08:40:50 +0200

    Ajout page google play

--- app/src/main/java/com/example/androidstudio/pages/Page_Parametre.java
+++ app/src/main/java/com/example/androidstudio/pages/Page_Parametre.java
@@ -2,16 +2,22 @@ package com.example.androidstudio.pages;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.androidstudio.MainActivity;
+import com.example.androidstudio.Page_Internet;
 import com.example.androidstudio.R;
 
+import static android.content.ContentValues.TAG;
+
 public class Page_Parametre extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,19 @@ public class Page_Parametre extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            Preference button = findPreference(getString(R.string.myCoolButton));
+            button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = new Intent(getActivity(), Page_Internet.class);
+                    intent.putExtra("af","https://play.google.com/store");
+                    startActivity(intent);
+                    return true;
+                }
+            });
         }
+
+
     }
-}
+        }
+
