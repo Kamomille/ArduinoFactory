c8dc47e763efe6d90954584c39fb89f4c7e0a095
Kamomille <camille.bayon-de-noyer@esme.fr>
2021-05-02 18:59:01 +0200

    onglet cours (pour Cédric)

--- app/src/main/java/com/example/androidstudio/cours/pour_cedric.java
+++ app/src/main/java/com/example/androidstudio/cours/pour_cedric.java
@@ -3,6 +3,8 @@ package com.example.androidstudio.cours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.androidstudio.R;
 import com.google.android.material.tabs.TabLayout;
@@ -14,7 +16,15 @@ public class pour_cedric extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pour_cedric);
 
+        LinearLayout layout_schema = (LinearLayout) findViewById(R.id.layout_schema);
+        LinearLayout layout_des = (LinearLayout) findViewById(R.id.layout_des);
+        LinearLayout layout_def = (LinearLayout) findViewById(R.id.layout_def);
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        layout_def.setVisibility(View.VISIBLE);
+        layout_des.setVisibility(View.INVISIBLE);
+        layout_schema.setVisibility(View.INVISIBLE);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -23,12 +33,21 @@ public class pour_cedric extends AppCompatActivity {
                 switch (position){
                     case 0:
                         // l'utilisateur clique sur definition
+                        layout_def.setVisibility(View.VISIBLE);
+                        layout_des.setVisibility(View.INVISIBLE);
+                        layout_schema.setVisibility(View.INVISIBLE);
                         return;
                     case 1:
                         // l'utilisateur clique sur definition
+                        layout_def.setVisibility(View.INVISIBLE);
+                        layout_des.setVisibility(View.VISIBLE);
+                        layout_schema.setVisibility(View.INVISIBLE);
                         return;
                     case 2:
                         // l'utilisateur clique sur definition
+                        layout_def.setVisibility(View.INVISIBLE);
+                        layout_des.setVisibility(View.INVISIBLE);
+                        layout_schema.setVisibility(View.VISIBLE);
                         return;
                     default: return; }
             }
