d4d25ac7ba2612e0a7d76671bf057b61b878dcd0
Julien <julien.gouban@esme.fr>
2025-09-03 13:44:09 +0200

    Enlever quelques warning sur la page MainActivity et ajout du son quand on clique qui avait disparu.
    Signed-off-by: Julien <julien.gouban@esme.fr>

--- app/src/main/java/com/ArduinoFactory/androidstudio/MainActivity.java
+++ app/src/main/java/com/ArduinoFactory/androidstudio/MainActivity.java
@@ -4,191 +4,164 @@ import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.ArduinoFactory.androidstudio.achat.Achat_Main;
-import com.ArduinoFactory.androidstudio.pages.Page_Contact;
-import com.ArduinoFactory.androidstudio.pages.Page_Menu_Cours;
 import com.ArduinoFactory.androidstudio.favoris.Page_Favoris;
 import com.ArduinoFactory.androidstudio.nouveaute.Page_Notification;
+import com.ArduinoFactory.androidstudio.pages.Page_Contact;
+import com.ArduinoFactory.androidstudio.pages.Page_Menu_Cours;
 import com.ArduinoFactory.androidstudio.pages.Page_Outils;
 import com.ArduinoFactory.androidstudio.pages.Page_Parametre;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-//import com.google.firebase.messaging.FirebaseMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton buttonParametre;
-    private ImageButton imageCours;
-    private ImageButton imageAchat;
-    private ImageButton imageContact;
     private SoundPool soundPool;
-    private AudioManager audioManager;
-    // Maximumn sound stream.
-    private static final int MAX_STREAMS = 100;
-    // Stream type.
-    private static final int streamType = AudioManager.STREAM_MUSIC;
-    private boolean loaded;
     private int soundIdBouton;
+    private boolean loaded = false;
     private float volume;
 
-
-    // Pour gérer les fragment (dasboard, home, notif) -------------------------------------------------------N
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        // Cacher la barre du haut
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+
+        // Initialiser les boutons
         Button buttonOutils = findViewById(R.id.buttonOutils);
         Button buttonCours = findViewById(R.id.buttonCours);
         Button buttonAchat = findViewById(R.id.buttonAchat);
         Button buttonContact = findViewById(R.id.buttonContacter);
-        ImageButton imageOutils = findViewById(R.id.imageOutils);
-        imageOutils = findViewById(R.id.imageCours);
-        imageOutils = findViewById(R.id.imageAchat);
-        imageOutils = findViewById(R.id.imageContacter);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view);
+        ImageButton buttonParametre = findViewById(R.id.buttonParametre);
+        ImageButton imageCours = findViewById(R.id.imageCours);
+        ImageButton imageAchat = findViewById(R.id.imageAchat);
+        ImageButton imageContact = findViewById(R.id.imageContacter);
 
-        // cacher la barre du haut
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-        // AudioManager audio settings for adjusting the volume
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        // Associer les clics
+        buttonOutils.setOnClickListener(this::onClickOutils);
+        buttonCours.setOnClickListener(this::onClickCours);
+        buttonAchat.setOnClickListener(this::onClickAchat);
+        buttonContact.setOnClickListener(this::onClickContacter);
+        buttonParametre.setOnClickListener(this::onClickParametre);
 
-        // Current volumn Index of particular stream type.
-        float currentVolumeIndex = (float) audioManager.getStreamVolume(streamType);
+        imageCours.setOnClickListener(this::onClickCours);
+        imageAchat.setOnClickListener(this::onClickAchat);
+        imageContact.setOnClickListener(this::onClickContacter);
 
-        // Get the maximum volume index for a particular stream type.
-        float maxVolumeIndex = (float) audioManager.getStreamMaxVolume(streamType);
+        // Initialiser le SoundPool
+        initSound();
 
-        // Volumn (0 --> 1)
-        this.volume = currentVolumeIndex / maxVolumeIndex;
-
-        // Suggests an audio stream whose volume should be changed by
-        // the hardware volume controls.
-        this.setVolumeControlStream(streamType);
-
-        // For Android SDK >= 21
-        if (Build.VERSION.SDK_INT >= 21) {
-            AudioAttributes audioAttrib = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_GAME)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .build();
-
-            SoundPool.Builder builder = new SoundPool.Builder();
-            builder.setAudioAttributes(audioAttrib).setMaxStreams(MAX_STREAMS);
-
-            this.soundPool = builder.build();
-        }
-        // for Android SDK < 21
-        else {
-            // SoundPool(int maxStreams, int streamType, int srcQuality)
-            this.soundPool = new SoundPool(MAX_STREAMS, AudioManager.STREAM_MUSIC, 0);
-        }
-
-        // When Sound Pool load complete.
-        this.soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                loaded = true;
+        // Navigation inférieure
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_dashboard) {
+                openFavoris();
+                return true;
+            } else if (itemId == R.id.navigation_notifications) {
+                openNotification();
+                return true;
             }
+            return false;
         });
+    }
 
-        // Load sound file (destroy.wav) into SoundPool.
-        this.soundIdBouton = this.soundPool.load(this, R.raw.son_bouton, 1);
+    private void initSound() {
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        float currentVolumeIndex = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolumeIndex = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = currentVolumeIndex / maxVolumeIndex;
 
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
+        AudioAttributes audioAttrib = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
 
+        soundPool = new SoundPool.Builder()
+                .setAudioAttributes(audioAttrib)
+                .setMaxStreams(1)
+                .build();
 
-    // Pour gérer la navigation avec les fragments (dasboard, home, notif) -----------------------------------------------
+        soundPool.setOnLoadCompleteListener((soundPool, sampleId, status) -> loaded = true);
+        soundIdBouton = soundPool.load(this, R.raw.son_bouton, 1);
+    }
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Fragment selectedFragment = null;
-                        if (item.getItemId() == R.id.navigation_dashboard) {
-                            openFavoris();
-                        }
-                        if (item.getItemId() == R.id.navigation_notifications) {
-                            openNotification();
-                        }
-                        //getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_container,selectedFragment);
-                        return false;
-                    }
-                });
-        // Pour faire fonctionner les boutons et les imageBoutons -----------------------------------------------
-}
-    public void onClickOutils (View view){
+    private void playSound() {
+        if (loaded && soundPool != null) {
+            soundPool.play(soundIdBouton, volume, volume, 1, 0, 1f);
+        }
+    }
+
+    // Méthodes de clic
+    public void onClickOutils(View view) {
         playSound();
         openActivtity_outils();
     }
-    public void onClickCours (View view){
+
+    public void onClickCours(View view) {
         playSound();
         openActivtity_cours();
     }
-    public void onClickAchat (View view){
+
+    public void onClickAchat(View view) {
         playSound();
         openActivtity_achat();
     }
-    public void onClickContacter (View view){
+
+    public void onClickContacter(View view) {
         playSound();
         openActivtity_contacter();
     }
-    public void onClickParametre (View view){
+
+    public void onClickParametre(View view) {
         playSound();
         openActivtity_parametre();
     }
 
-    public void openActivtity_outils(){
-        Intent intent = new Intent(this, Page_Outils.class);
-        startActivity(intent);
+    // Navigation vers d'autres pages
+    public void openActivtity_outils() {
+        startActivity(new Intent(this, Page_Outils.class));
     }
-    public void openActivtity_cours(){
-        Intent intent = new Intent(this, Page_Menu_Cours.class);
-        startActivity(intent);
+
+    public void openActivtity_cours() {
+        startActivity(new Intent(this, Page_Menu_Cours.class));
     }
-    public void openActivtity_achat(){
-        Intent intent = new Intent(this, Achat_Main.class);
-        startActivity(intent);
+
+    public void openActivtity_achat() {
+        startActivity(new Intent(this, Achat_Main.class));
     }
-    public void openActivtity_contacter(){
-        Intent intent = new Intent(this, Page_Contact.class);
-        startActivity(intent);
+
+    public void openActivtity_contacter() {
+        startActivity(new Intent(this, Page_Contact.class));
     }
+
     public void openActivtity_parametre() {
-        Intent intent = new Intent(this, Page_Parametre.class);
-        startActivity(intent);
+        startActivity(new Intent(this, Page_Parametre.class));
     }
+
     public void openFavoris() {
+        Intent intent = new Intent(this, Page_Favoris.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
         finish();
-        this.startActivity(new Intent(this, Page_Favoris.class));
-        this.overridePendingTransition(0, 0);
-    }
-    public void openNotification(){
-        finish();
-        this.startActivity(new Intent(this, Page_Notification.class));
-        this.overridePendingTransition(0, 0);
-    }
-    public void playSound( )  {
-        if(loaded)  {
-            float leftVolumn = volume;
-            float rightVolumn = volume;
-            // Play sound of gunfire. Returns the ID of the new stream.
-            int streamId = this.soundPool.play(this.soundIdBouton,leftVolumn, rightVolumn, 1, 0, 1f);
-        }
     }
 
+    public void openNotification() {
+        Intent intent = new Intent(this, Page_Notification.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
     }
-
+}
