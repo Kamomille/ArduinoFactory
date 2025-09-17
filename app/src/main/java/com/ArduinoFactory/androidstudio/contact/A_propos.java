ad0565b04ec062254582de768053680e2502bdfe
Julien <julien.gouban@esme.fr>
2025-09-17 11:02:19 +0200

    Amélioration de la page Apropos pour y ajouter le lien du livre ainsi qu'enlever les remerciements.
    Signed-off-by: Julien <julien.gouban@esme.fr>

--- app/src/main/java/com/ArduinoFactory/androidstudio/contact/A_propos.java
+++ app/src/main/java/com/ArduinoFactory/androidstudio/contact/A_propos.java
@@ -20,41 +20,23 @@ public class A_propos extends AppCompatActivity {
 
         // bouton retour
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
 
-        TextView qui = findViewById(R.id.qui);
-        qui.setPaintFlags(qui.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        // ===== LIVRE =====
+        TextView livreFr = findViewById(R.id.lienduLivre);
 
-        TextView remerciement = findViewById(R.id.remerciement);
-        remerciement.setPaintFlags(remerciement.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        livreFr.setOnClickListener(v -> openUrl(getString(R.string.lien_livre)));
 
-        TextView nom1 = findViewById(R.id.nom1);
-        nom1.setText(getString(R.string.nom1));
-
-        TextView nom2 = findViewById(R.id.nom2);
-        nom2.setText(getString(R.string.nom2));
-
-        TextView nom3 = findViewById(R.id.nom3);
-        nom3.setText(getString(R.string.nom3));
-
-        TextView nom4 = findViewById(R.id.nom4);
-        nom4.setText(getString(R.string.nom4));
+        // ===== JOIN US =====
+        TextView lien = findViewById(R.id.lien);
+        TextView youtube = findViewById(R.id.youtube);
+        TextView instagram = findViewById(R.id.instragram);
 
         TextView rejoignez = findViewById(R.id.rejoignez);
         rejoignez.setPaintFlags(rejoignez.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
-        TextView lien = findViewById(R.id.lien);
-        lien.setPaintFlags(lien.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         lien.setOnClickListener(v -> openUrl(getString(R.string.lien_url)));
-
-        TextView youtube = findViewById(R.id.youtube);
-        youtube.setPaintFlags(youtube.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         youtube.setOnClickListener(v -> openUrl(getString(R.string.lien_youtube)));
-
-        TextView instagram = findViewById(R.id.instragram);
-        instagram.setPaintFlags(instagram.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         instagram.setOnClickListener(v -> openUrl(getString(R.string.lien_instagram)));
     }
 
